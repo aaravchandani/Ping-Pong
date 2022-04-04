9ce094585c810df1e5ea035706bd6821244feeff
aaravchandani <81931219+aaravchandani@users.noreply.github.com>
2022-04-03 17:46:26 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -24,9 +24,14 @@ var ball = {
     dx:3,
     dy:3
 }
-
+function restart(){
+  pcscore = 0;
+  playerscore = 0;
+  loop()
+  
+}
 function preload() {
-  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  ball_touch_paddle = loadSound("ball_touch_paddel.wav");
   missed = loadSound("missed.wav");
 }
 
@@ -84,7 +89,7 @@ function draw(){
    fill(250,0,0);
     stroke(0,0,250);
     strokeWeight(0.5);
-   paddle1Y = mouseY; 
+   paddle1Y = rightwristY; 
    rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
    
    
@@ -155,12 +160,14 @@ function move(){
    }
   if (ball.x-2.5*ball.r/2< 0){
   if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
+    ball_touch_paddle.play();
     ball.dx = -ball.dx+0.5; 
   }
   else{
     pcscore++;
     reset();
     navigator.vibrate(100);
+    missed.play()
   }
 }
 if(pcscore ==4){
@@ -171,7 +178,7 @@ if(pcscore ==4){
     stroke("white");
     textSize(25)
     text("Game Over!☹☹",width/2,height/2);
-    text("Reload The Page!",width/2,height/2+30)
+    text("Press the restart button to play again!",width/2,height/2+30)
     noLoop();
     pcscore = 0;
 }
